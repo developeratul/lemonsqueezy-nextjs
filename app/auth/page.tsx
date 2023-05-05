d2cdac26836ed16c20aeed0c980b66a5d6e7d580
DevR <azammmgol@gmail.com>
2023-05-05 10:43:44 +0600

    small fix in the tailwind config

---
+++ app/auth/page.tsx
@@ -0,0 +1,7 @@
+export default function Auth() {
+  return (
+    <div>
+      <h1>Auth login</h1>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main>
+    <main className="min-h-screen flex flex-col justify-center items-center">
       <h1>Hello world</h1>
     </main>
   );
