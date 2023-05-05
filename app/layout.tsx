56e7e963f232cb5542888a62ec9829f4ca7dd3b6
DevR <azammmgol@gmail.com>
2023-05-05 10:40:29 +0600

    update next.js version

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import { cn } from "~/lib/utils";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(inter.className)}>{children}</body>
     </html>
   );
 }
